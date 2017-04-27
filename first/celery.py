a4c9c17a2b031e5da25bd3c32eb3186b511d6d84
Janusz Kamieński <janusz.kamienski@hummingbird.pl>
2017-04-27 10:42:02 +0200

    celery use start

---
+++ first/celery.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, unicode_literals
+from celery import Celery
+
+app = Celery('app', backend='rpc://', broker = 'pyamqp://admin:mypass@localhost:5672//', include=['first.tasks'])
+
+# Optional configuration, see the application user guide.
+app.conf.update(
+    result_expires=3600,
+)
+
+if __name__ == '__main__':
+    app.start()
